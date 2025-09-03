c89cee6ba8d74a5b30c0a5d922e5c55bd05ca3c6
Mijail Pulgar <mijail86@gmail.com>
2025-09-03 17:23:48 -0500

    tailwind + shadcn ui + google fonts

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <h1 className="font-montserrat">Hola Mundo</h1>
+      <h1 className="font-montserrat font-thin">Hola Mundo</h1>
+      <h1 className="font-montserrat font-normal">Hola Mundo</h1>
+      <h1 className="font-montserrat font-bold">Hola Mundo</h1>
+      <Button>Hola mundo</Button>
+    </div>
+  );
 };
 
 export default HomePage;
