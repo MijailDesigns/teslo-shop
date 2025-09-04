088f9300c23ef8f554bc552482ff49f6e267b0c4
Mijail Pulgar <mijail86@gmail.com>
2025-09-04 16:20:38 -0500

    tanstack query - devTools y axios

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -1,8 +1,17 @@
 import { RouterProvider } from "react-router";
 import { appRouter } from "./app.router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const TesloShopApp = () => {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={appRouter} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default TesloShopApp;
