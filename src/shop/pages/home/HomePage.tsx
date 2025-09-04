7e74c7211bf70aea8dd7799ce348774a4bd5a3e5
Mijail Pulgar <mijail86@gmail.com>
2025-09-04 09:32:40 -0500

    homepage - paginacion y productos

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,16 @@
+import CustomPagination from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/product.mock";
 import CustomJumbotron from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
 
 const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Todos los productos" />
+
+      <ProductsGrid products={products} />
+
+      <CustomPagination totalPages={7} />
     </>
   );
 };
