d8b8692e6e5b5578b251cd205c731a1c555aa29d
Mijail Pulgar <mijail86@gmail.com>
2025-09-04 17:26:47 -0500

    aplicar paginacion

--- src/shop/hooks/useProducts.tsx
+++ src/shop/hooks/useProducts.tsx
@@ -1,12 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProductsAction } from "../actions/get-products.action";
+import { useSearchParams } from "react-router";
 
 const useProducts = () => {
   // todo: viene logica
 
+  const [searchParams] = useSearchParams();
+
+  const limit = searchParams.get("limit") || 9;
+  const page = searchParams.get("page") || 1;
+
+  const offset = (Number(page) - 1) * Number(limit);
+
   return useQuery({
-    queryKey: ["products"],
-    queryFn: getProductsAction,
+    queryKey: ["products", { offset, limit }],
+    queryFn: () =>
+      getProductsAction({
+        limit: isNaN(+limit) ? 9 : limit,
+        offset: isNaN(offset) ? 0 : offset,
+      }),
+    staleTime: 1000 * 60 * 5,
   });
 };
 
