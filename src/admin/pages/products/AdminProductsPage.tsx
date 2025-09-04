572d288d2dc599544458617645f1f0faf0b124c8
Mijail Pulgar <mijail86@gmail.com>
2025-09-04 15:23:29 -0500

    diesño - ProductPage

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -21,11 +21,13 @@ const AdminProductsPage = () => {
           subtitle="Aqui puedes ver y administrar tus productos"
         />
 
-        <Link to="/admin/products/new">
-          <Button>
-            <PlusIcon /> Nuevo producto
-          </Button>
-        </Link>
+        <div className="flex justify-end mb-10 gap-4">
+          <Link to="/admin/products/new">
+            <Button>
+              <PlusIcon /> Nuevo producto
+            </Button>
+          </Link>
+        </div>
       </div>
 
       <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
