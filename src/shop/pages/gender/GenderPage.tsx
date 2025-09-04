dce168bbdf27fea78f3d26f4667fa1ec1df0b09f
Mijail Pulgar <mijail86@gmail.com>
2025-09-04 11:37:44 -0500

    navegacion entre pantallas

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,5 +1,23 @@
+import CustomPagination from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/product.mock";
+import CustomJumbotron from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
+import { useParams } from "react-router";
+
 const GenderPage = () => {
-  return <div>GenderPage</div>;
+  const { gender } = useParams();
+  const genderLabel =
+    gender === "men" ? "Hombres" : gender === "women" ? "Mujeres" : "Niños";
+
+  return (
+    <>
+      <CustomJumbotron title={`Productos para ${genderLabel}`} />
+
+      <ProductsGrid products={products} />
+
+      <CustomPagination totalPages={7} />
+    </>
+  );
 };
 
 export default GenderPage;
