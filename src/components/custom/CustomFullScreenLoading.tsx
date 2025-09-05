fc2249a4cb85ad81ce3b2ece612ebc994b68846a
Mijail Pulgar <mijail86@gmail.com>
2025-09-05 11:52:38 -0500

    verificar estado de la autenticacion

---
+++ src/components/custom/CustomFullScreenLoading.tsx
@@ -0,0 +1,12 @@
+const CustomFullScreenLoading = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <div className="h-10 w-10 animate-spin rounded-full border-4 border-primary border-t-transparent" />
+        <p className="text-lg font-mdeium">Esper un momento ...</p>
+      </div>
+    </div>
+  );
+};
+
+export default CustomFullScreenLoading;
