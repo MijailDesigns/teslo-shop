09834246eebb86de4097c0d1146ce956b28b1f6d
Mijail Pulgar <mijail86@gmail.com>
2025-09-05 21:52:07 -0500

    admin - busqueda, paginacion y productos

--- src/admin/components/AdminHeader.tsx
+++ src/admin/components/AdminHeader.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef, type KeyboardEvent } from "react";
 import { Search, Bell, MessageSquare, Settings } from "lucide-react";
+import { useNavigate } from "react-router";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSarch = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== "Enter") return;
+    const query = inputRef.current?.value;
+
+    if (!query) {
+      navigate("/admin/products");
+      return;
+    }
+    navigate(`/admin/products?query=${query}`);
+  };
+
   return (
     <header className="bg-white border-b border-gray-200 px-6 py-4 h-18">
       <div className="flex items-center justify-between">
@@ -14,6 +29,8 @@ const Header: React.FC = () => {
             />
             <input
               type="text"
+              ref={inputRef}
+              onKeyDown={handleSarch}
               placeholder="Search..."
               className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
             />
