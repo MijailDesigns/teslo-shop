9806e56412a097e2c23720d588576400df9de316
Mijail Pulgar <mijail86@gmail.com>
2025-09-08 12:12:14 -0500

    accion - actualizar o crear producto

---
+++ src/admin/actions/create-update-product.action.ts
@@ -0,0 +1,28 @@
+import { tesloApi } from "@/api/tesloApi";
+import type { Product } from "@/interfaces/product.interface";
+
+export const createUpdateProductAction = async (
+  productLike: Partial<Product>
+): Promise<Product> => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { id, user, images = [], ...rest } = productLike;
+
+  const isCreating = id === "new";
+
+  rest.stock = Number(rest.stock || 0);
+  rest.price = Number(rest.price || 0);
+
+  const { data } = await tesloApi<Product>({
+    url: isCreating ? "/products" : `/product/${id}`,
+    method: isCreating ? "POST" : "PATCH",
+    data: rest,
+  });
+
+  return {
+    ...data,
+    images: data.images.map((img) => {
+      if (img.includes("http")) return img;
+      return `${import.meta.env.VITE_API_URL}/files/product/${img}`;
+    }),
+  };
+};
